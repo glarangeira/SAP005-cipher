959ed1b00a34143cb9af61e90e937b02564df770
g.larangeira <glarangeirag@gmail.com>
2020-11-04 13:18:32 -0300

    mensagem

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,40 @@
-//let originalMessage = ' ';
-
-//function myFunction() {
- // originalMessage = document.getElementById("myTextarea1").value;
- // document.getElementById("demo").innerHTML = x;
+//const cipher = {
 //}
+//export default cipher;
 
-const cipher = function(encode, decode) {
+function encode() {
+    let originalMessage = document.getElementById("inCifrar").value;
+    let offset = parseInt(document.getElementById("numero").value);
 
-};
+    let final = "";
+    for (let i = 0; i < originalMessage.length; i++) {
+        let valorChar = originalMessage[i].charCodeAt();
+        if (valorChar >= 65 && valorChar <= 90) {
+            final += String.fromCharCode(((valorChar - 65 + offset) % 26) + 65);
+        } else if (valorChar >= 97 && valorChar <= 122) {
+            final += String.fromCharCode(((valorChar - 97 + offset) % 26) + 97);
+        } else {
+            final += originalMessage[i];
+        }
+    }
+    document.getElementById('demo1').innerHTML = final;
 
-export default cipher;
+}
+function decode() {
+    let originalMessage = document.getElementById("inDecifrar").value;
+    let offset = parseInt(document.getElementById("numero").value);
+
+    let final = "";
+    for (let i = 0; i < originalMessage.length; i++) {
+        let valorChar = originalMessage[i].charCodeAt();
+        if (valorChar >= 65 && valorChar <= 90) {
+            final += String.fromCharCode(((valorChar - 65 - offset + 26) % 26) + 65);
+        } else if (valorChar >= 97 && valorChar <= 122) {
+            final += String.fromCharCode(((valorChar - 97 - offset + 26) % 26) + 97);
+        } else {
+            final += originalMessage[i];
+        }
+    }
+    document.getElementById('demo2').innerHTML = final;
+
+}
